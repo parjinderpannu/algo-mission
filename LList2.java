06dbddc32d86d9d1bad957c4105f7d4cf9cb91b0
Parjinder Pannu <ppannu@cisco.com>
2018-10-05 23:40:59 -0700

    Lab4 without last questions

--- LList2.java
+++ LList2.java
@@ -1,281 +1,295 @@
 /**
-   A class that implements the ADT list by using a chain of nodes.
-   The chain has both a head reference and a tail reference.
-   @author Frank M. Carrano
-   @version 3.0
-*/
-public class LList2<T> implements ListInterface<T>
-{
+ * A class that implements the ADT list by using a chain of nodes.
+ * The chain has both a head reference and a tail reference.
+ *
+ * @author Frank M. Carrano
+ * @version 3.0
+ */
+public class LList2<T> implements ListInterface<T> {
 	private Node firstNode; // head reference to first node
 	private Node lastNode;  // tail reference to last node
-	private int  numberOfEntries;
-
-	public LList2()
-	{
-		clear();
+	private int numberOfEntries;
+ 
+	public LList2() {
+	   clear();
 	} // end default constructor
-
+ 
 	public final void clear() // NOTICE clear is not final in interface and that is OK
 	{
-		firstNode = null;
-  		lastNode = null;
-		numberOfEntries = 0;
+	   firstNode = null;
+	   lastNode = null;
+	   numberOfEntries = 0;
 	} // end clear
-  
-	public void add(T newEntry) 	  // OutOfMemoryError possible
+ 
+	public void add(T newEntry)     // OutOfMemoryError possible
 	{
-		Node newNode = new Node(newEntry); // create new node
-
-		if (isEmpty())
-			firstNode = newNode;
-		else
-			lastNode.setNextNode(newNode);
-		
-  		lastNode = newNode;
-  		numberOfEntries++;
+	   Node newNode = new Node(newEntry); // create new node
+ 
+	   if (isEmpty())
+		  firstNode = newNode;
+	   else
+		  lastNode.setNextNode(newNode);
+ 
+	   lastNode = newNode;
+	   numberOfEntries++;
 	}  // end add
-
-   public boolean add(int newPosition, T newEntry)  // OutOfMemoryError possible	                                                 
+ 
+	public boolean add(int newPosition, T newEntry)  // OutOfMemoryError possible	                                                 
 	{
-      boolean isSuccessful = true;
-
-      if ((newPosition >= 1) && (newPosition <= numberOfEntries + 1)) 
-      {
-         Node newNode = new Node(newEntry);
-
-         if (isEmpty())
-         {
-            firstNode = newNode;
-            lastNode = newNode;
-         }
-         else if (newPosition == 1)
-         {
-            newNode.setNextNode(firstNode);
-            firstNode = newNode;
-         }
-         else if (newPosition == numberOfEntries + 1)
-         {
-            lastNode.setNextNode(newNode);
-            lastNode = newNode;
-         } 
-         else
-         {
-            Node nodeBefore = getNodeAt(newPosition - 1);
-            Node nodeAfter = nodeBefore.getNextNode();
-            newNode.setNextNode(nodeAfter);
-            nodeBefore.setNextNode(newNode);
-         } // end if	    
-         numberOfEntries++;
-      }
-      else
-         isSuccessful = false;
-       
+	   boolean isSuccessful = true;
+ 
+	   if ((newPosition >= 1) && (newPosition <= numberOfEntries + 1)) {
+		  Node newNode = new Node(newEntry);
+ 
+		  if (isEmpty()) {
+			 firstNode = newNode;
+			 lastNode = newNode;
+		  } else if (newPosition == 1) {
+			 newNode.setNextNode(firstNode);
+			 firstNode = newNode;
+		  } else if (newPosition == numberOfEntries + 1) {
+			 lastNode.setNextNode(newNode);
+			 lastNode = newNode;
+		  } else {
+			 Node nodeBefore = getNodeAt(newPosition - 1);
+			 Node nodeAfter = nodeBefore.getNextNode();
+			 newNode.setNextNode(nodeAfter);
+			 nodeBefore.setNextNode(newNode);
+		  } // end if	    
+		  numberOfEntries++;
+	   } else
+		  isSuccessful = false;
+ 
 	   return isSuccessful;
 	} // end add
-
-	public T remove(int givenPosition)
-	{
-      T result = null;                 // return value
-
-      if ((givenPosition >= 1) && (givenPosition <= numberOfEntries))
-      {
-         assert !isEmpty();
-         if (givenPosition == 1)        // case 1: remove first entry
-         {
-            result = firstNode.getData();     // save entry to be removed 
-            firstNode = firstNode.getNextNode();
-            if (numberOfEntries == 1)
-               lastNode = null; // solitary entry was removed
-            }
-            else                           // case 2: givenPosition > 1
-            {
-               Node nodeBefore = getNodeAt(givenPosition - 1);
-               Node nodeToRemove = nodeBefore.getNextNode();
-               Node nodeAfter = nodeToRemove.getNextNode();
-               nodeBefore.setNextNode(nodeAfter);  // disconnect the node to be removed
-               result = nodeToRemove.getData();  // save entry to be removed
-
-               if (givenPosition == numberOfEntries)
-                  lastNode = nodeBefore; // last node was removed
-         } // end if
-
-         numberOfEntries--;
-      } // end if
-
-      return result;                   // return removed entry, or 
-                                      // null if operation fails
+ 
+	public T remove(int givenPosition) {
+	   T result = null;                 // return value
+ 
+	   if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+		  assert !isEmpty();
+		  if (givenPosition == 1)        // case 1: remove first entry
+		  {
+			 result = firstNode.getData();     // save entry to be removed 
+			 firstNode = firstNode.getNextNode();
+			 if (numberOfEntries == 1)
+				lastNode = null; // solitary entry was removed
+		  } else                           // case 2: givenPosition > 1
+		  {
+			 Node nodeBefore = getNodeAt(givenPosition - 1);
+			 Node nodeToRemove = nodeBefore.getNextNode();
+			 Node nodeAfter = nodeToRemove.getNextNode();
+			 nodeBefore.setNextNode(nodeAfter);  // disconnect the node to be removed
+			 result = nodeToRemove.getData();  // save entry to be removed
+ 
+			 if (givenPosition == numberOfEntries)
+				lastNode = nodeBefore; // last node was removed
+		  } // end if
+ 
+		  numberOfEntries--;
+	   } // end if
+ 
+	   return result;                   // return removed entry, or 
+	   // null if operation fails
 	} // end remove
-
-	public boolean replace(int givenPosition, T newEntry)
-	{
-		boolean isSuccessful = true;
-
-      if ((givenPosition >= 1) && (givenPosition <= numberOfEntries))
-      {   
-      	assert !isEmpty();
-
-			Node desiredNode = getNodeAt(givenPosition);
-			desiredNode.setData(newEntry);
-      }    
-		else
-			isSuccessful = false;
-			
-		return isSuccessful;
-   } // end replace
-
-   public T getEntry(int givenPosition)
-   {
-      T result = null;  // result to return
-      
-		if ((givenPosition >= 1) && (givenPosition <= numberOfEntries))
-		{
-			assert !isEmpty();
-         result = getNodeAt(givenPosition).getData();
-     	} // end if
-      
-      return result;
-   } // end getEntry
-
-	public boolean contains(T anEntry)
-	{
-		boolean found = false;
-		Node currentNode = firstNode;
-		
-		while (!found && (currentNode != null))
-		{
-			if (anEntry.equals(currentNode.getData()))
-				found = true;
-			else
-				currentNode = currentNode.getNextNode();
-		} // end while
-		
-		return found;
+ 
+	public boolean replace(int givenPosition, T newEntry) {
+	   boolean isSuccessful = true;
+ 
+	   if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+		  assert !isEmpty();
+ 
+		  Node desiredNode = getNodeAt(givenPosition);
+		  desiredNode.setData(newEntry);
+	   } else
+		  isSuccessful = false;
+ 
+	   return isSuccessful;
+	} // end replace
+ 
+	public T getEntry(int givenPosition) {
+	   T result = null;  // result to return
+ 
+	   if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+		  assert !isEmpty();
+		  result = getNodeAt(givenPosition).getData();
+	   } // end if
+ 
+	   return result;
+	} // end getEntry
+ 
+	public boolean contains(T anEntry) {
+	   boolean found = false;
+	   Node currentNode = firstNode;
+ 
+	   while (!found && (currentNode != null)) {
+		  if (anEntry.equals(currentNode.getData()))
+			 found = true;
+		  else
+			 currentNode = currentNode.getNextNode();
+	   } // end while
+ 
+	   return found;
 	} // end contains
-
-   public int getLength()
-   {
-      return numberOfEntries;
-   } // end getLength
-
-   public boolean isEmpty()
-   {
-   		boolean result;
-   		
-      	if (numberOfEntries == 0) // or getLength() == 0
-      	{
-      		assert firstNode == null;
-      		result = true;
-      	}
-      	else
-      	{
-      		assert firstNode != null;
-      		result = false;
-      	} // end if
-      	
-      	return result;
-   } // end isEmpty
-	
-   public T[] toArray()
-   {
-      // the cast is safe because the new array contains null entries
-      @SuppressWarnings("unchecked")
-      T[] result = (T[])new Object[numberOfEntries]; // warning: [unchecked] unchecked cast
-
-	  int index = 0;
-     Node currentNode = firstNode;
-	  while ((index < numberOfEntries) && (currentNode != null))
-	  { 
-	    result[index] = currentNode.getData();
-	    currentNode = currentNode.getNextNode();
-       index++; 
-	  } // end while
-     
-     return result;
-   } // end toArray
-
+ 
+	public int getLength() {
+	   return numberOfEntries;
+	} // end getLength
+ 
+	public boolean isEmpty() {
+	   boolean result;
+ 
+	   if (numberOfEntries == 0) // or getLength() == 0
+	   {
+		  assert firstNode == null;
+		  result = true;
+	   } else {
+		  assert firstNode != null;
+		  result = false;
+	   } // end if
+ 
+	   return result;
+	} // end isEmpty
+ 
+	public T[] toArray() {
+	   // the cast is safe because the new array contains null entries
+	   @SuppressWarnings("unchecked")
+	   T[] result = (T[]) new Object[numberOfEntries]; // warning: [unchecked] unchecked cast
+ 
+	   int index = 0;
+	   Node currentNode = firstNode;
+	   while ((index < numberOfEntries) && (currentNode != null)) {
+		  result[index] = currentNode.getData();
+		  currentNode = currentNode.getNextNode();
+		  index++;
+	   } // end while
+ 
+	   return result;
+	} // end toArray
+ 
 	// Returns a reference to the node at a given position.
-	// Precondition:  List is not empty; 1 <= givenPosition <= numberOfEntries.	
-	private Node getNodeAt(int givenPosition)
-	{
-		assert (firstNode != null) && (1 <= givenPosition) && (givenPosition <= numberOfEntries);
-		Node currentNode = firstNode;
-		
-      if (givenPosition == numberOfEntries)
-         currentNode = lastNode;
-      else if (givenPosition > 1)      // traverse the chain to locate the desired node
-		{
-         for (int counter = 1; counter < givenPosition; counter++)
-            currentNode = currentNode.getNextNode();
-		} // end if
-      
-		assert currentNode != null;
-		return currentNode;
+	// Precondition:  List is not empty; 1 <= givenPosition <= numberOfEntries.
+	private Node getNodeAt(int givenPosition) {
+	   assert (firstNode != null) && (1 <= givenPosition) && (givenPosition <= numberOfEntries);
+	   Node currentNode = firstNode;
+ 
+	   if (givenPosition == numberOfEntries)
+		  currentNode = lastNode;
+	   else if (givenPosition > 1)      // traverse the chain to locate the desired node
+	   {
+		  for (int counter = 1; counter < givenPosition; counter++)
+			 currentNode = currentNode.getNextNode();
+	   } // end if
+ 
+	   assert currentNode != null;
+	   return currentNode;
 	} // end getNodeAt
-
+ 
 	// Lab 3
 	// Problem 1
-	int getIndex(T item) {
-	
-		return 0;
+	int getLastIndex(T item) {
+	   assert (firstNode != null);
+	   if(this.isEmpty())
+		  return -1;
+	   int index = -1;
+ 
+	   if (item == getNodeAt(numberOfEntries).getData())
+		  return numberOfEntries;
+	   else {
+		  for(int counter=1; counter <=numberOfEntries; counter++){
+			 if(item == getNodeAt(counter).getData()){
+				index = counter;
+			 }
+		  }
+	   }
+	   return index;
 	}
+ 
 	// Problem 2
 	int removeEvery(T item) {
-	
-		return 0;
+	   if(this.isEmpty())
+		  return 0;
+	   int count = 0;
+ 
+	   for(int i=1; i<=numberOfEntries;i++){
+		  if(item == getNodeAt(i).getData()){
+			 this.remove(i);
+			 count++;
+		  }
+	   }
+ 
+	   return count;
 	}
+ 
 	// Problem 3
-	boolean equals(Object other) {
-	
-		return false;
+	public boolean equals(Object other) {
+ 
+	   if ((other == this) || (this == null && other == null))
+	   return true;
+	   if ((this == null && other != null) || (this != null && other == null)
+		  || (other.getClass() != this.getClass()))
+		  return false;
+ 
+	   LList2<T> otherList = (LList2<T>) other;
+ 
+	   if (this.getLength() != otherList.getLength()) {
+		  return false;
+	   } else {
+		  for (int i = 1; i <= otherList.getLength(); i++) {
+			 if (this.getNodeAt(i).getData() != otherList.getNodeAt(i).getData()) {
+				return false;
+			 }
+ 
+		  }
+	   }
+ 
+	   return true;
 	}
-	
+ 
 	//Problem 4
 	boolean contains2(T anEntry) {
-	
-		return false;
+ 
+	   return false;
 	}
+ 
 	private boolean contains(T anEntry, Node startNode) {
-		
-		return false;
+ 
+	   return false;
 	}
-	
-	
-	private class Node 
-	{
-		private T data;  // data portion
-		private Node next;  // next to next node
-
-		private Node(T dataPortion)//  PRIVATE or PUBLIC is OK
-		{
-			data = dataPortion;
-			next = null;	
-		} // end constructor
-
-		private Node(T dataPortion, Node nextNode)//  PRIVATE or PUBLIC is OK
-		{
-			data = dataPortion;
-			next = nextNode;	
-		} // end constructor
-
-		private T getData()
-		{
-			return data;
-		} // end getData
-		
-		private void setData(T newData)
-		{
-			data = newData;
-		} // end setData
-		
-		private Node getNextNode()
-		{
-			return next;
-		} // end getNextNode
-		
-		private void setNextNode(Node nextNode)
-		{
-			next = nextNode;
-		} // end setNextNode
+ 
+ 
+	private class Node {
+	   private T data;  // data portion
+	   private Node next;  // next to next node
+ 
+	   private Node(T dataPortion)//  PRIVATE or PUBLIC is OK
+	   {
+		  data = dataPortion;
+		  next = null;
+	   } // end constructor
+ 
+	   private Node(T dataPortion, Node nextNode)//  PRIVATE or PUBLIC is OK
+	   {
+		  data = dataPortion;
+		  next = nextNode;
+	   } // end constructor
+ 
+	   private T getData() {
+		  return data;
+	   } // end getData
+ 
+	   private void setData(T newData) {
+		  data = newData;
+	   } // end setData
+ 
+	   private Node getNextNode() {
+		  return next;
+	   } // end getNextNode
+ 
+	   private void setNextNode(Node nextNode) {
+		  next = nextNode;
+	   } // end setNextNode
 	} // end Node
-} // end LList2
-
+ } // end LList2
+ 
+ 
--- ListClient.java
+++ ListClient.java
@@ -1,1 +1,100 @@
-public class ListClient {	public static void main(String[] args) 	{		testList();	}  // end main	public static void testList() 	{      ListInterface<String> runnerList = new AList<String>();//  runnerList has only methods in ListInterface      runnerList.add("16"); // winner      runnerList.add(" 4"); // second place      runnerList.add("33"); // third place      runnerList.add("27"); // fourth place      displayList(runnerList);   } // end testList      public static void displayList(ListInterface<String> list)   {      int numberOfEntries = list.getLength();      System.out.println("The list contains " + numberOfEntries +                         " entries, as follows:");      for (int position = 1; position <= numberOfEntries; position++)         System.out.println(list.getEntry(position) +                            " is entry " + position);      System.out.println();   } // end displayList} // end ListClient/**/
+public class ListClient 
+{
+	public static void main(String[] args) 
+	{
+		testList();
+	}  // end main
+
+	public static void testList() 
+	{
+      AList l1 = new AList<String>();
+      AList l2 = new AList<String>();
+      LList2 ll1 = new LList2();
+      LList2 ll2 = new LList2();
+
+//  runnerList has only methods in ListInterface
+
+      l1.add("1"); // winner
+      l1.add("2"); // second place
+      l1.add("4"); // third place
+      l1.add("4"); // fourth place
+      l1.add("5");
+
+      System.out.println("************ Testing Array public int getLastIndex(T item)************");
+      System.out.println("************ Following are the contents of l1             ************");
+      displayList(l1);
+      System.out.println("l1.getLastIndex(\"4\") = "+l1.getLastIndex("4") );
+
+      System.out.println("************ Testing Array public int removeEvery(T item)  ************");
+      System.out.println("l1.removeEvery(\"4\") ="+l1.removeEvery("4"));
+      System.out.println("************ Following are the contents of l1             ************");
+      displayList(l1);
+
+      l2.add("1");
+      l2.add("2");
+      l2.add("3");
+
+      System.out.println("************ Testing Array public boolean equals(Object other)   ************");
+      System.out.println("************ Following are the contents of l2             ************");
+      displayList(l2);
+      System.out.println("l1.equals(l2) = "+l1.equals(l2));
+
+      System.out.println("l1.removeEvery(\"5\") ="+l1.removeEvery("5"));
+      System.out.println("************ Following are the contents of l1             ************");
+      displayList(l1);
+      System.out.println("l2.removeEvery(\"3\") ="+l2.removeEvery("3"));
+      System.out.println("************ Following are the contents of l2             ************");
+      displayList(l2);
+      System.out.println("l1.equals(l2) = "+l1.equals(l2));
+
+      ll1.add("1"); // winner
+      ll1.add("2"); // second place
+      ll1.add("4"); // third place
+      ll1.add("4"); // fourth place
+      ll1.add("5");
+
+      System.out.println("************ Testing List public int getLastIndex(T item)************");
+      System.out.println("************ Following are the contents of ll1             ************");
+      displayList(ll1);
+      System.out.println("ll1.getLastIndex(\"4\") = "+ll1.getLastIndex("4") );
+
+      System.out.println("************ Testing Array public int removeEvery(T item)  ************");
+      System.out.println("ll1.removeEvery(\"4\") ="+ll1.removeEvery("4"));
+      System.out.println("************ Following are the contents of ll1             ************");
+      displayList(ll1);
+
+      ll2.add("1");
+      ll2.add("2");
+      ll2.add("3");
+
+      System.out.println("************ Testing Array public boolean equals(Object other)   ************");
+      System.out.println("************ Following are the contents of ll2             ************");
+      displayList(ll2);
+      System.out.println("ll1.equals(ll2) = "+ll1.equals(ll2));
+
+      System.out.println("ll1.removeEvery(\"5\") ="+ll1.removeEvery("5"));
+      System.out.println("************ Following are the contents of ll1             ************");
+      displayList(ll1);
+      System.out.println("ll2.removeEvery(\"3\") ="+ll2.removeEvery("3"));
+      System.out.println("************ Following are the contents of ll2             ************");
+      displayList(ll2);
+      System.out.println("ll1.equals(ll2) = "+ll1.equals(ll2));
+
+      System.out.println();
+   } // end testList
+   
+   public static void displayList(ListInterface<String> list)
+   {
+      int numberOfEntries = list.getLength();
+      System.out.println("The list contains " + numberOfEntries +
+                         " entries, as follows:");
+      for (int position = 1; position <= numberOfEntries; position++)
+         System.out.println(list.getEntry(position) +
+                            " is entry " + position);
+      System.out.println();
+   } // end displayList
+} // end ListClient
+
+/*
+
+*/
